2390855b0213e09a9c6eda17ad0e7762bfa38396
Alphonsa <alphonsamonalisa19@gmail.com>
2023-05-26 01:49:35 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ const App = () => {
              <input type="text" placeholder='search...' id='search_input'
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)} />
-               <img
+               <img id='searchicon'
           src={'https://gist.githubusercontent.com/adrianhajdin/997a8cdf94234e889fa47be89a4759f1/raw/f13e5a9a0d1e299696aa4a0fe3a0026fa2a387f7/search.svg'}
           alt="search"
           onClick={() => searchMovies(searchTerm)}
